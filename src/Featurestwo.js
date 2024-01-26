4cc26b30e95c68659ecb4b03fa91937c4b2b5509
kaungpyaesoneaunggic <kaungpyaesoneaung@gicjp.com>
2024-01-26 16:46:50 +0630

    Carousel fixed

--- src/Featurestwo.js
+++ src/Featurestwo.js
@@ -3,12 +3,20 @@ import { accentCyan, accentGrey, accentRed, accentYellow, primaryColor } from '.
 import featuregraph from './images/featuresgraph.png'
 import MotoCard from './components/MotoCard'
 import { faCheck, faRefresh, faSuitcase, faTasks } from '@fortawesome/free-solid-svg-icons'
+import { Carousel } from 'react-bootstrap'
 
 export default function Featurestwo() {
   return (
     <div className='d-flex flex-wrap flex-lg-nowrap justify-content-between py-5' style={{ backgroundColor: accentGrey, margin: '20px 0px 20px 0px', padding: '30px 10px 50px 10px' }}>
       <div className='card col-lg-6 order-lg-1 order-2'>
-        <img className="img-fluid" src={featuregraph} alt="Feature Graph" />
+        <Carousel>
+          <Carousel.Item>
+            <img src={featuregraph}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src={featuregraph}/>
+          </Carousel.Item>
+        </Carousel>
       </div>
       <div className='col-lg-5 order-lg-2 order-1 d-flex flex-column-reverse'>
         <MotoCard
